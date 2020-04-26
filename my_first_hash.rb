8f41cee33b41bedb31f56d007f1c3b496dced607
Antonio Espinoza <aespinoza777@gmail.com>
2020-04-26 16:55:48 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -21,6 +21,11 @@ def retrieval
 end
 
 def adding
+shipping_manifest = {
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3
+  }
   shipping_manifest["muskets"]=2
   # add 2 muskets to the shipping_manifest hash below
  shipping_manifest["gun powder"] = 4
